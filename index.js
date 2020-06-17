e8ba945ff942b40321e56e4aa5866eeac7d2d8a3
Seanbon0611 <seand0611@gmail.com>
2020-06-17 10:07:42 -0700

    updating master with both changes that don't break app

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
+let today, currentMonth, currentYear, days, months, years, tbodyTag, calendarTitle, yearsSelections, monthsSelections;
 
-const usersURL = "http://localhost:3000/api/v1/users";
-const notesURL = "http://localhost:3000/api/v1/notes";
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+years = ["2020", "2021", "2022", "2023", "2024", "2025"]
 
-const today = new Date();
-const currentMonth = today.getMonth();
-const currentYear = today.getFullYear();
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-const years = ["2020", "2021", "2022", "2023", "2024", "2025"]
-
-// grab calendar div
-const calendarDiv = document.getElementById('calendar-container');
-// grab calendar button
-const calendarButton = document.getElementById("calendar-btn");
 // grab calendar body 
-const tbodyTag = document.getElementById("calendar-body");
+tbodyTag = document.getElementById("calendar-body");
 // grab title (month - year)
-const calendarTitle = document.getElementById("monthAndYear");
+calendarTitle = document.getElementById("monthAndYear");
 // grab dropdown years
-const yearsSelections = document.getElementById("year");
+yearsSelections = document.getElementById("year");
 // grab dropdown month
-const monthsSelections = document.getElementById("month");
-// grab new note button
-const newNoteButton = document.getElementById("note-btn");
-// grab new note div
-const newNoteDiv = document.getElementById("new-note");
+monthsSelections = document.getElementById("month");
+
+
+
+
+
 
 const appContainer = document.querySelector('#app');
 isSignedIn = false
@@ -54,13 +49,14 @@ previousButtonClickListener();
 nextButtonClickListener();
 dropDownListener();
 listenToNewNoteClick()
-newNoteButtonListener();
-// fetchBackend();
 }
 
 function listenToNewNoteClick() {
   const newNoteBtn = document.querySelector('#note-btn')
-  newNoteBtn.addEventListener('click', (e) => {
+  const newNoteDiv = document.getElementById("new-note");
+  const calendarDiv = document.getElementById('calendar-container');
+  const calendarButton = document.getElementById("calendar-btn");
+  newNoteBtn.addEventListener('click', (event) => {
     // once clicked disable the button
     event.target.disabled = true;
     // once clicked display textarea
@@ -203,69 +199,64 @@ function handleSignIn(signInData) {
   })
 }
 
-//NOTE FUNCTION
-
-
 //CALENDAR FUNCTIONS
 
-// function fetchBackend() {
-//     fetch(baseURL)
-//       .then((response) => response.json())
-//       .then((userData) => console.log(userData));
-// }
-
 function renderCalendarAfterClicked() {
-    calendarButton.addEventListener("click", function(event) {
-        // once clicked disable the button
-        event.target.disabled = true;
-        // once clicked display calendar
-        calendarDiv.style.display = "block";
-        // once clicked undisable new note button
-        newNoteButton.disabled = false;
-        // once clicked hide note textarea
-        newNoteDiv.style.display = "none";
-    });
+  const calendarButton = document.getElementById("calendar-btn");
+  const calendarDiv = document.getElementById('calendar-container');
+  const newNoteBtn = document.querySelector('#note-btn')
+  const newNoteDiv = document.getElementById("new-note");
+  calendarButton.addEventListener("click", function(event) {
+      // once clicked disable the button
+      event.target.disabled = true;
+      // once clicked display calendar
+      calendarDiv.style.display = "block";
+      // once clicked undisable new note button
+      newNoteBtn.disabled = false;
+      // once clicked hide note textarea
+      newNoteDiv.style.display = "none";
+  });
 }
 
 function displayCalendar(month, year) {
-    //// DISPLAYING CALENDAR BODY ////
-    const firstDay = (new Date(year, month)).getDay();
-    const daysInMonth = 32 - new Date(year, month, 32).getDate(); 
-    // clearing body
-    tbodyTag.innerHTML = "";   
-    // assign month - year to calendar title
-    calendarTitle.innerText = months[month] + " " + year;
-    yearsSelections.value = year;
-    monthsSelections.value = month;
-    // generating dates
-    let date = 1;
-    for (let counter = 0; counter < 6; counter++) {
-        let calendarBodyRow = document.createElement("tr");
-        // filling calendar with dates
-        for (let i = 0; i < 7; i++) {
-            if (counter === 0 && i < firstDay) {
-                // adding cell
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                calendarBodyRow.appendChild(cell);
-            } else if (date > daysInMonth) {
-                break;
-            } else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    // color today date
-                    cell.style.background = "#B0E0E6";
-                } 
-                cell.appendChild(cellText);
-                calendarBodyRow.appendChild(cell);
-                date++;
-            }      
-        }
-        // add each row to calendar body
-        tbodyTag.appendChild(calendarBodyRow);
-    }
+  //// DISPLAYING CALENDAR BODY ////
+  const firstDay = (new Date(year, month)).getDay();
+  const daysInMonth = 32 - new Date(year, month, 32).getDate(); 
+  // clearing body
+  tbodyTag.innerHTML = "";   
+  // assign month - year to calendar title
+  calendarTitle.innerText = months[month] + " " + year;
+  yearsSelections.value = year;
+  monthsSelections.value = month;
+  // generating dates
+  let date = 1;
+  for (let counter = 0; counter < 6; counter++) {
+      let calendarBodyRow = document.createElement("tr");
+      // filling calendar with dates
+      for (let i = 0; i < 7; i++) {
+          if (counter === 0 && i < firstDay) {
+              // adding cell
+              let cell = document.createElement("td");
+              let cellText = document.createTextNode("");
+              cell.appendChild(cellText);
+              calendarBodyRow.appendChild(cell);
+          } else if (date > daysInMonth) {
+              break;
+          } else {
+              let cell = document.createElement("td");
+              let cellText = document.createTextNode(date);
+              if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                  // color today's date
+                  cell.style.background = "#B0E0E6";
+              } 
+              cell.appendChild(cellText);
+              calendarBodyRow.appendChild(cell);
+              date++;
+          }      
+      }
+      // add each row to calendar body.
+      tbodyTag.appendChild(calendarBodyRow);
+  }
 }
 
 function previousButtonClickListener() {
