ae66df1196980f76b1ea64fa3755548543a58e47
Seanbon0611 <seand0611@gmail.com>
2020-06-16 18:01:06 -0700

    worked on randering different forms/pages, handled signin auth

--- calendar.js
+++ calendar.js
@@ -1,81 +1,176 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
+// let today;
+// let currentMonth;
+// let currentYear;
+// let days;
+// let months;
 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// document.addEventListener("DOMContentLoaded", function() {
+//     today = new Date();
+//     currentMonth = today.getMonth();
+//     currentYear = today.getFullYear();
+//     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
+//     renderCalendarAfterClicked();
+// })
 
+// function renderCalendarAfterClicked() {
+//     const calendarButton = document.getElementById("calendar-btn");
+//     calendarButton.addEventListener("click", function(event) {
+//         // once clicked disable the button
+//         event.target.disabled = true;
+//         // grab calendar div
+//         const calendarDiv = document.getElementById('calendar-container');
+//         // add calendar card
+//         const calendarCard = document.createElement("div");
+//         calendarCard.setAttribute("class", "card");
+//         calendarDiv.append(calendarCard);
+//         // title (month - year)
+//         const calendarTitle = document.createElement("h3");
+//         calendarTitle.setAttribute("class", "card-header");
+//         calendarTitle.setAttribute("id", "monthAndYear");
+//         calendarCard.append(calendarTitle);
+//         // calendar table
+//         const calendarTable = document.createElement("table");
+//         calendarTable.setAttribute("class", "table table-bordered table-responsive-sm");
+//         calendarTable.setAttribute("id", "calendar");
+//         calendarCard.append(calendarTable);
+//         //// header with days
+//         const theadTag = document.createElement("thead");
+//         calendarTable.append(theadTag);
+//         const trTag = document.createElement("tr");
+//         theadTag.append(trTag);
+//         const thTag1 = document.createElement("th");
+//         thTag1.innerText = days[0];
+//         const thTag2 = document.createElement("th");
+//         thTag2.innerText = days[1];
+//         const thTag3 = document.createElement("th");
+//         thTag3.innerText = days[2];
+//         const thTag4 = document.createElement("th");
+//         thTag4.innerText = days[3];
+//         const thTag5 = document.createElement("th");
+//         thTag5.innerText = days[4];
+//         const thTag6 = document.createElement("th");
+//         thTag6.innerText = days[5];
+//         const thTag7 = document.createElement("th");
+//         thTag7.innerText = days[6];
+//         trTag.append(thTag1, thTag2,thTag3, thTag4, thTag5, thTag6, thTag7);
+//         // body 
+//         const tbodyTag = document.createElement("tbody");
+//         tbodyTag.setAttribute("id", "calendar-body");
+//         calendarTable.append(tbodyTag);
+//         // previous and next buttons container
+//         const previousNextButtonsContainer = document.createElement("div");
+//         previousNextButtonsContainer.setAttribute("class", "form-inline")
+//         calendarCard.append(previousNextButtonsContainer);
+//         //// add previous button
+//         const previousButton = document.createElement("button");
+//         previousButton.setAttribute("class", "btn btn-outline-info col-sm-6");
+//         previousButton.setAttribute("id", "previous");
+//         previousButton.innerText = "<<";
+//         previousNextButtonsContainer.append(previousButton);
+//         //// add next button
+//         const nextButton = document.createElement("button");
+//         nextButton.setAttribute("class", "btn btn-outline-info col-sm-6");
+//         nextButton.setAttribute("id", "next");
+//         nextButton.innerText = ">>";
+//         previousNextButtonsContainer.append(nextButton);
+//         // dropdown
+//         const dropDownForm = document.createElement("form");
+//         dropDownForm.setAttribute("class", "form-inline");
+//         calendarCard.append(dropDownForm);
+//         //// dropdown months
+//         const monthsSelections = document.createElement("select");
+//         monthsSelections.setAttribute("class", "form-control col-sm-6");
+//         monthsSelections.setAttribute("id", "month");
+//         monthsSelections.setAttribute("name", "month");
+//         dropDownForm.append(monthsSelections);
+//         //// add each month
+//         const month1 = document.createElement("option");
+//         month1.innerText = months[0];
+//         const month2 = document.createElement("option");
+//         month2.innerText = months[1];
+//         const month3 = document.createElement("option");
+//         month3.innerText = months[2];
+//         const month4 = document.createElement("option");
+//         month4.innerText = months[3];
+//         const month5 = document.createElement("option");
+//         month5.innerText = months[4];
+//         const month6 = document.createElement("option");
+//         month6.innerText = months[5];
+//         const month7 = document.createElement("option");
+//         month7.innerText = months[6];
+//         const month8 = document.createElement("option");
+//         month8.innerText = months[7];
+//         const month9 = document.createElement("option");
+//         month9.innerText = months[8];
+//         const month10 = document.createElement("option");
+//         month10.innerText = months[9];
+//         const month11 = document.createElement("option");
+//         month11.innerText = months[10];
+//         const month12 = document.createElement("option");
+//         month12.innerText = months[11];
+//         monthsSelections.append(month1, month2, month3, month4, month5, month6, month7, month8, month9, month10, month11 ,month12);
+//         //// dropdown years
+//         const yearsSelections = document.createElement("select");
+//         yearsSelections.setAttribute("class", "form-control col-sm-6");
+//         yearsSelections.setAttribute("name", "year");
+//         yearsSelections.setAttribute("id", "year");
+//         dropDownForm.append(yearsSelections);
+//         //// add each year
+//         const year1 = document.createElement("option");
+//         year1.innerText = "2020";
+//         const year2 = document.createElement("option");
+//         year2.innerText = "2021";
+//         const year3 = document.createElement("option");
+//         year3.innerText = "2022";
+//         const year4 = document.createElement("option");
+//         year4.innerText = "2023";
+//         const year5 = document.createElement("option");
+//         year5.innerText = "2024";
+//         const year6 = document.createElement("option");
+//         year6.innerText = "2025";
+//         yearsSelections.append(year1, year2, year3, year4, year5, year6);
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
-                } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
-    }
-
-}
+//         // DISPLAYING CALENDAR BODY  
+//         const firstDay = (new Date(currentYear, currentMonth)).getDay();
+//         const daysInMonth = 32 - new Date(currentYear, currentMonth, 32).getDate(); 
+//         // clearing body
+//         tbodyTag.innerHTML = "";   
+//         // assign month - year to calendar title
+//         calendarTitle.innerText = months[currentMonth] + " " + currentYear;
+//         yearsSelections.value = currentYear;
+//         monthsSelections.value = currentMonth;
+//         // generating dates
+//         let date = 1;
+//         for (let counter = 0; counter < 6; counter++) {
+//             let calendarBodyRow = document.createElement("tr");
+//             // filling calendar with dates
+//             for (let i = 0; i < 7; i++) {
+//                 if (counter === 0 && i < firstDay) {
+//                     // adding cell
+//                     let cell = document.createElement("td");
+//                     let cellText = document.createTextNode("");
+//                     cell.appendChild(cellText);
+//                     calendarBodyRow.appendChild(cell);
+//                 }
+//                 else if (date > daysInMonth) {
+//                     break;
+//                 }
+//                 else {
+//                     let cell = document.createElement("td");
+//                     let cellText = document.createTextNode(date);
+//                     if (date === today.getDate() && currentYear === today.getFullYear() && currentMonth === today.getMonth()) {
+//                         // color today's date
+//                         cell.classList.add("bg-info");
+//                     } 
+//                     cell.appendChild(cellText);
+//                     calendarBodyRow.appendChild(cell);
+//                     date++;
+//                 }      
+//             }
+//             // add each row to calendar body.
+//             tbodyTag.appendChild(calendarBodyRow);
+//         }
+//     });
+// }
